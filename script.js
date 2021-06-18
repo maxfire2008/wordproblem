a1bc74bb1f3bb62f6a943222bf226117a7de9059
maxfire2008 <22722054+maxfire2008@users.noreply.github.com>
2021-06-18 13:55:16 +1000

    modified script.js

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
+Array.prototype.remove = function() {
+    var what, a = arguments, L = a.length, ax;
+    while (L && this.length) {
+        what = a[--L];
+        while ((ax = this.indexOf(what)) !== -1) {
+            this.splice(ax, 1);
+        }
+    }
+    return this;
+};
+
 var avalibleVariableNames = ['Red', 'HoneyDew', 'Beige', 'DarkTurquoise', 'DarkMagenta', 'DarkGreen', 'Crimson', 'Bisque', 'Magenta', 'SeaGreen', 'Snow', 'Grey', 'LightSeaGreen', 'DimGrey', 'Navy', 'LightGreen', 'MintCream', 'Linen', 'LavenderBlush', 'Turquoise', 'Plum', 'DarkViolet', 'MediumSeaGreen', 'MistyRose', 'DarkOrchid', 'LightPink', 'DarkSlateGray', 'HotPink', 'RosyBrown', 'DarkSalmon', 'MediumTurquoise', 'Orange', 'DarkGoldenRod', 'IndianRed', 'Azure', 'MediumSlateBlue', 'LightGrey', 'SandyBrown', 'BlueViolet', 'LightBlue', 'LightSlateGrey', 'BurlyWood', 'PaleTurquoise', 'SeaShell', 'LightSkyBlue', 'PaleVioletRed', 'Ivory', 'LightYellow', 'SlateGrey', 'MediumPurple', 'PeachPuff', 'SkyBlue', 'NavajoWhite', 'AntiqueWhite', 'LawnGreen', 'MidnightBlue', 'Fuchsia', 'GreenYellow', 'White', 'CornflowerBlue', 'Gold', 'DarkGrey', 'RoyalBlue', 'Salmon', 'DarkKhaki', 'CadetBlue', 'GhostWhite', 'DarkCyan', 'PaleGreen', 'Orchid', 'Green', 'Indigo', 'YellowGreen', 'MediumAquaMarine', 'Khaki', 'DarkBlue', 'FloralWhite', 'Coral', 'Cyan', 'LimeGreen', 'LightSlateGray', 'LemonChiffon', 'PapayaWhip', 'Purple', 'Olive', 'SpringGreen', 'Violet', 'MediumBlue', 'DarkRed', 'BlanchedAlmond', 'LightGoldenRodYellow', 'Blue', 'OrangeRed', 'MediumSpringGreen', 'Brown', 'DeepSkyBlue', 'Sienna', 'Wheat', 'DarkSlateGrey', 'DarkSeaGreen', 'DeepPink', 'Tomato', 'DarkGray', 'Gray', 'FireBrick', 'Moccasin', 'Silver', 'GoldenRod', 'LightSalmon', 'Peru', 'DarkOliveGreen', 'Chocolate', 'AliceBlue', 'DimGray', 'OldLace', 'Black', 'ForestGreen', 'LightCoral', 'Tan', 'Chartreuse', 'LightCyan', 'PowderBlue', 'Lavender', 'Gainsboro', 'OliveDrab', 'Aquamarine', 'Lime', 'Thistle', 'RebeccaPurple', 'PaleGoldenRod', 'MediumVioletRed', 'Cornsilk', 'SteelBlue', 'Maroon', 'DarkSlateBlue', 'Teal', 'LightGray', 'SaddleBrown', 'LightSteelBlue', 'DarkOrange', 'SlateBlue', 'SlateGray', 'Aqua', 'Yellow', 'MediumOrchid', 'Pink', 'WhiteSmoke', 'DodgerBlue'];
 var variables = [];
 var variableCount = 0;
+var variableTypes = [
+    {
+        "regex": /^rand\([0-9]{1,},[0-9]{1,}(,[0-9]{1,}){0,1}\)$/,
+        "function": function (x) {//parseRandomNumber
+            //return random number
+            console.log("Process Random "+x);
+            return x;
+        },
+    }
+];
+
 function download(text, name, type) {
     var a = document.getElementById("saveButton");
     var file = new Blob([text], {type: type});
@@ -16,11 +38,16 @@ function setSaveButton() {
 	download(JSON.stringify({"text":textInput.value,"variables":variables,"answer":answerFormulaInput.value}), filename+'.wps', 'text/plain');
 }
 
-function parseRandomNumber(x) {//1,5,1
-    //return random number
+function parseVariable(x) {
+    for (i = 0; i < variableTypes.length; i++) {
+        if (x.match(variableTypes[i]["regex"])) {
+            variableTypes[i]["function"](x);
+        }
+    }
 }
 
-function parseVariable() {
+function updatePreview(variableName) {
+    document.getElementById(variableName+"");
 }
 
 function addVariable() {
@@ -46,10 +73,12 @@ function addVariable() {
     newRemoveButton = document.createElement("button");
     newRemoveButton.id = variableName+"RemoveButton";
     newRemoveButton.onclick = function (e) {
-        e.currentTarget.parentNode.remove();//  bug how do I get buttonElement
+        e.currentTarget.parentNode.remove();
     }
     newRemoveButton.textContent="-";
     newVariable.appendChild(newRemoveButton);
+    
+    variables.push(variableName);
     
 	variablesDiv.appendChild(newVariable);
     variableCount+=1;
